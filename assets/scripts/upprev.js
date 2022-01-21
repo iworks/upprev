59c00a1c72a5ad248afacb76e7f50b3abe9100e3
Marcin Pietrzak <marcin@iworks.pl>
2022-01-21 11:13:00 +0100

    prepare 4.0.5

--- assets/scripts/upprev.js
+++ assets/scripts/upprev.js
@@ -1,6 +1,6 @@
-/*! upPrev - v4.0.4
+/*! upPrev - v4.0.5
  * https://iworks.pl/
- * Copyright (c) 2021; * Licensed GPLv2+
+ * Copyright (c) 2022; * Licensed GPLv2+
  */
 jQuery(function($) {
     if ('undefined' == typeof(iworks_upprev)) {
